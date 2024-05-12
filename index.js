adfbd2dc73523af47a39da68ed91a4117cc6ffb6
Scoicaru Andrei Alexandru <andreiscoicaru2002@gmail.com>
2024-05-12 22:54:08 +0300

    added project files

---
+++ index.js
@@ -0,0 +1,180 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+const dbConfig = {
+  user: "postgres",
+  host: "localhost",
+  database: "movie_library",
+  password: "Masina1Masina123!",
+  port: 5432,
+};
+
+let errors = [];
+let isSearching = false;
+
+const db = new pg.Client(dbConfig);
+db.connect();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function getMovies() {
+  try {
+    const result = await db.query("SELECT * FROM movies ORDER BY rating DESC");
+    return result.rows;
+  } catch (err) {
+    console.error("Error getting movies:", err);
+    return [];
+  }
+}
+
+app.get("/", async (req, res) => {
+
+  errors = [];
+  isSearching = false;
+
+  try {
+    const movies = await getMovies();
+    res.render("index.ejs", {
+      movies,
+    });
+  } catch (err) {
+    console.error("Error getting movies:", err);
+  }
+  });
+
+  app.get("/add", async (req, res) => {
+      res.render("addMovie.ejs",{
+        errors,
+      });
+    });
+
+  app.post("/add", async (req, res) => {
+    const movieName = req.body.movieName;
+    const rating = req.body.rating;
+    const apiKey = "4c13b6d9";
+
+  try {
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieName)}`);
+    console.log(response.rows);
+    const data = response.data;
+    await db.query("INSERT INTO movies(title, description, rating, poster_src) VALUES ($1, $2, $3, $4)", [data.Title, data.Plot, rating, data.Poster]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error getting movies:", err);
+
+    errors = [];
+
+    if(err.routine === "_bt_check_unique") {
+      errors.push("Movie is already listed"); 
+    } else {
+      errors.push("Movie not found");
+    }
+
+    if(err.constraint === "check_value" || err.routine == "float8in_internal_opt_error") {
+      errors.push("Rating should be number between 0 and 10");
+    }
+
+    res.redirect("/add");
+  }
+});
+
+app.get("/add", async (req, res) => {
+  res.render("addMovie.ejs");
+});
+
+app.post("/delete/:id", async (req, res) => {
+  try {
+    await db.query("DELETE FROM movies WHERE id=$1", [req.params.id]);
+  } catch (err) {
+    console.error("Error getting movies:", err);
+  }
+  res.redirect("/");
+});
+
+app.get("/movie/:id", async (req, res) => {
+  try {
+    const result = await db.query("SELECT title FROM movies WHERE id=$1", [req.params.id]);
+    const movieName = result.rows[0].title;
+    res.redirect(`https://www.imdb.com/find?q=${encodeURIComponent(movieName)}`);
+  } catch (err) {
+    console.error("Error getting movies:", err);
+  }
+});
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+
+    const result = await db.query("SELECT title, rating FROM movies WHERE id=$1", [req.params.id]);
+    const movieTitle = result.rows[0].title;
+    const movieRating = result.rows[0].rating;
+
+    res.render("editRating.ejs", {
+      movieId: req.params.id,
+      movieTitle,    
+      movieRating,
+      errors
+    });
+  } catch (err) {
+
+    if(err.constraint === "check_value" || err.routine == "float8in_internal_opt_error") {
+      errors.push("Rating should be number between 0 and 10");
+    }
+    console.error("Error editing rating:", err);
+
+    res.redirect(`/edit/${movieId}`);
+
+  }
+});
+
+app.post("/edit/:id", async (req, res) => {
+  
+  const editedRating = req.body.editedRating;
+  const movieId = req.params.id;
+  
+  try {
+
+    await db.query("UPDATE movies SET rating=$1 WHERE id=$2", [editedRating, movieId]);
+    
+  } catch (err) {
+    console.error("Error editing rating:", err);
+  }
+
+  res.redirect("/");
+
+});
+
+app.get("/search", async (req, res) => {
+  
+  const search = String(req.query.search).trim().toLowerCase();
+  console.log("Search", search);
+
+  try {
+
+    const result = await db.query("SELECT * FROM movies WHERE LOWER(title) LIKE $1", ['%' + search + '%']);
+    const filteredMovies = result.rows;
+    
+    isSearching = true;
+
+    res.render("index.ejs", {
+      movies: filteredMovies,
+      search
+    });
+    
+  } catch (err) {
+    console.error("Error searching movies:", err);
+    res.redirect("/");
+  }
+
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
