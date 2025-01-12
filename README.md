02233a64518410b53ccf0df566e0146338556a62
scoic <andreiscoicaru2002@gmail.com>
2025-01-12 17:45:17 +0200

    fixed some bugs and added comments

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Link
-Access to the website is available at the following address, utilizing Render (https://render.com): 
+
+Access to the website is available at the following address, utilizing Render (https://render.com):
+
 - https://movie-library-web-app.onrender.com
 
 # Description
+
 This project is a Movie Library web application built with Node.js, Express.js, PostgreSQL, and EJS templating engine. It allows users to manage and rate movies they have watched. Key features include viewing, adding, editing, and deleting movies. Users can also search for movies by title. The application fetches movie details from the OMDb API and stores them in a PostgreSQL database. Error handling is implemented for duplicate movies and invalid ratings.
 
 # Key Features
+
 - View, add, edit, and delete movies
 - Search movies by title
 - Fetch movie details from OMDb API
@@ -14,6 +18,7 @@ This project is a Movie Library web application built with Node.js, Express.js, 
 - Rate limiting for optimized server performance
 
 # Technologies Used
+
 - Node.js
 - Express.js
 - PostgreSQL
@@ -22,15 +27,15 @@ This project is a Movie Library web application built with Node.js, Express.js, 
 - Body Parser middleware
 
 # Preview
+
 - Home Page
-  
+
 ![home_page](https://github.com/user-attachments/assets/bdefb78f-0dac-4ac1-9d67-de13cd5b61da)
 
 - Add New Movie Page
-  
+
 ![add_movie_page](https://github.com/user-attachments/assets/75b540c5-54ce-411c-b07b-271ba93df328)
 
 - Search Result Page
-  
-![search_result_page](https://github.com/user-attachments/assets/1821521e-3861-4ace-aa71-b97e21fd807b)
 
+![search_result_page](https://github.com/user-attachments/assets/1821521e-3861-4ace-aa71-b97e21fd807b)
--- index.js
+++ index.js
@@ -9,24 +9,31 @@ env.config();
 const app = express();
 const port = 3000;
 
+// Database configuration for connecting to PostgreSQL
 const dbConfig = {
   user: process.env.USER,
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 let errors = [];
 let isSearching = false;
 
+// Connect to PostgreSQL database
 const db = new pg.Client(dbConfig);
 db.connect();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Function to fetch movies from the database
 async function getMovies() {
   try {
     const result = await db.query("SELECT * FROM movies ORDER BY rating DESC");
@@ -37,8 +44,8 @@ async function getMovies() {
   }
 }
 
+// Display all movies
 app.get("/", async (req, res) => {
-
   errors = [];
   isSearching = false;
 
@@ -46,42 +53,53 @@ app.get("/", async (req, res) => {
     const movies = await getMovies();
     res.render("index.ejs", {
       movies,
-      isSearching
+      isSearching,
     });
   } catch (err) {
     console.error("Error getting movies:", err);
   }
+});
+
+// Render page for adding a movie
+app.get("/add", async (req, res) => {
+  res.render("addMovie.ejs", {
+    errors,
   });
+});
 
-  app.get("/add", async (req, res) => {
-      res.render("addMovie.ejs",{
-        errors,
-      });
-    });
-
-  app.post("/add", rateLimitMiddleware, async (req, res) => {
-    const movieName = req.body.movieName;
-    const rating = req.body.rating;
-    const apiKey = process.env.API_KEY;
+// Add a new movie
+app.post("/add", rateLimitMiddleware, async (req, res) => {
+  const movieName = req.body.movieName;
+  const rating = req.body.rating;
+  const apiKey = process.env.API_KEY;
 
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieName)}`);
+    const response = await axios.get(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieName)}`,
+    );
     console.log(response.rows);
     const data = response.data;
-    await db.query("INSERT INTO movies(title, description, rating, poster_src) VALUES ($1, $2, $3, $4)", [data.Title, data.Plot, rating, data.Poster]);
+    await db.query(
+      "INSERT INTO movies(title, description, rating, poster_src) VALUES ($1, $2, $3, $4)",
+      [data.Title, data.Plot, rating, data.Poster],
+    );
     res.redirect("/");
   } catch (err) {
     console.error("Error getting movies:", err);
 
     errors = [];
 
-    if(err.routine === "_bt_check_unique") {
-      errors.push("Movie is already listed"); 
+    // Check if the movie already exists
+    if (err.routine === "_bt_check_unique") {
+      errors.push("Movie is already listed");
     } else {
       errors.push("Movie not found");
     }
 
-    if(err.constraint === "check_value" || err.routine == "float8in_internal_opt_error") {
+    if (
+      err.constraint === "check_value" ||
+      err.routine == "float8in_internal_opt_error"
+    ) {
       errors.push("Rating should be number between 0 and 10");
     }
 
@@ -89,10 +107,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.get("/add", async (req, res) => {
-  res.render("addMovie.ejs");
-});
-
+// Delete a movie
 app.post("/delete/:id", async (req, res) => {
   try {
     await db.query("DELETE FROM movies WHERE id=$1", [req.params.id]);
@@ -102,90 +117,102 @@ app.post("/delete/:id", async (req, res) => {
   res.redirect("/");
 });
 
+// Redirect to IMDB page
 app.get("/movie/:id", async (req, res) => {
   try {
-    const result = await db.query("SELECT title FROM movies WHERE id=$1", [req.params.id]);
-    const movieName = result.rows[0].title;
-    res.redirect(`https://www.imdb.com/find?q=${encodeURIComponent(movieName)}`);
+    const result = await db.query("SELECT title FROM movies WHERE id=$1", [
+      req.params.id,
+    ]);
+    const movieName = result.rows[0].title; // Extract movie title
+    res.redirect(
+      `https://www.imdb.com/find?q=${encodeURIComponent(movieName)}`,
+    );
   } catch (err) {
     console.error("Error getting movies:", err);
   }
 });
 
+// Edit movie rating
 app.get("/edit/:id", async (req, res) => {
   try {
-
-    const result = await db.query("SELECT title, rating FROM movies WHERE id=$1", [req.params.id]);
+    const result = await db.query(
+      "SELECT title, rating FROM movies WHERE id=$1",
+      [req.params.id],
+    );
     const movieTitle = result.rows[0].title;
     const movieRating = result.rows[0].rating;
 
     res.render("editRating.ejs", {
       movieId: req.params.id,
-      movieTitle,    
+      movieTitle,
       movieRating,
-      errors
+      errors,
     });
   } catch (err) {
     console.log(err);
   }
 });
 
+// Update movie rating
 app.post("/edit/:id", async (req, res) => {
-  
   const editedRating = req.body.editedRating;
   const movieId = req.params.id;
   errors = [];
-  
-  try {
 
-    await db.query("UPDATE movies SET rating=$1 WHERE id=$2", [editedRating, movieId]);
+  try {
+    await db.query("UPDATE movies SET rating=$1 WHERE id=$2", [
+      editedRating,
+      movieId,
+    ]);
     res.redirect("/");
-    
   } catch (err) {
     console.error("Error editing rating:", err);
-    if(err.constraint === "check_value" || err.routine == "float8in_internal_opt_error") {
+    if (
+      err.constraint === "check_value" ||
+      err.routine == "float8in_internal_opt_error"
+    ) {
       errors.push("Rating should be number between 0 and 10");
     }
-    console.error("Error editing rating:", err);
-
     res.redirect(`/edit/${movieId}`);
   }
 });
 
+// Search for movies
 app.get("/search", async (req, res) => {
-  
   const search = String(req.query.search).trim().toLowerCase();
 
   try {
-
-    const result = await db.query("SELECT * FROM movies WHERE LOWER(title) LIKE $1", ['%' + search + '%']);
+    const result = await db.query(
+      "SELECT * FROM movies WHERE LOWER(title) LIKE $1",
+      ["%" + search + "%"],
+    );
     const filteredMovies = result.rows;
-    
+
     isSearching = true;
 
     res.render("index.ejs", {
       movies: filteredMovies,
-      search
+      search,
     });
-    
   } catch (err) {
     console.error("Error searching movies:", err);
     res.redirect("/");
   }
-
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
+  console.log(`Server running on port ${port}`);
+});
+
+// Rate limiting middleware
 const requestCounts = {};
 
 function rateLimitMiddleware(req, res, next) {
-  const ip = req.ip; 
-  const now = Date.now(); 
-  const windowMs = 15 * 60 * 1000; 
-  const maxRequests = 50;
+  const ip = req.ip;
+  const now = Date.now();
+  const windowMs = 15 * 60 * 1000; // Time window for rate limiting
+  const maxRequests = 50; // Maximum allowed requests per time window
 
   if (!requestCounts[ip]) {
     requestCounts[ip] = [];
@@ -196,7 +223,9 @@ function rateLimitMiddleware(req, res, next) {
   });
 
   if (requestCounts[ip].length >= maxRequests) {
-    return res.status(429).send('Too many movies added from this IP, please try again later');
+    return res
+      .status(429)
+      .send("Too many movies added from this IP, please try again later");
   }
 
   requestCounts[ip].push(now);
